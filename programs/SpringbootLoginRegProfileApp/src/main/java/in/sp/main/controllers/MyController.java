ecf0734de438e3c2854c87b5b148cfbbe26fef04
Nishita-Maheshwari <nishita.maheshwari55@gmail.com>
2023-07-09 11:19:30 +0530

    Add files via upload

---
+++ programs/SpringbootLoginRegProfileApp/src/main/java/in/sp/main/controllers/MyController.java
@@ -0,0 +1,44 @@
+package in.sp.main.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController
+{
+	@GetMapping("/")
+	public String openHomePage()
+	{
+		return "home-page";
+	}
+	
+	@GetMapping("/homePage")
+	public String openHomePagee()
+	{
+		return "home-page";
+	}
+	
+	@GetMapping("/aboutUsPage")
+	public String openAboutUsPage()
+	{
+		return "about-us-page";
+	}
+	
+	@GetMapping("/contactUsPage")
+	public String openContactUsPage()
+	{
+		return "contact-us-page";
+	}
+	
+	@GetMapping("/loginPage")
+	public String openLoginPage()
+	{
+		return "login-page";
+	}
+	
+	@GetMapping("/registerPage")
+	public String openRegisterPage()
+	{
+		return "register-page";
+	}
+}
---
+++ programs/SpringbootLoginRegProfileApp/src/main/webapp/views/menubar.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Insert title here</title>
+		<link rel="stylesheet" type="text/css" href="/css/style.css" />
+	</head>
+	<body>
+		<div class="menubar_div">
+			<a href="homePage">Home</a>
+			<a href="aboutUsPage">About Us</a>
+			<a href="contactUsPage">Contact Us</a>
+			<a href="loginPage">Login</a>
+			<a href="registerPage">Register</a>
+		</div>
+	</body>
+</html>
